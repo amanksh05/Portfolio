f4b2138eeb2083cf68955f6ad51da1ef2de999f3
Aman Kumar <amanksh05@gmail.com>
2024-07-25 23:43:34 +0530

    Nav modified

--- app/navigation.js
+++ app/navigation.js
@@ -1,15 +1,17 @@
+"use client"
 import Link from 'next/link';
-
+import { usePathname } from 'next/navigation';
 const Navigation = () => {
+    const pathName = usePathname();
     return (
-        <div className='flex flex-row justify-between items-center p-4 px-8 border-b border-zinc-800 '>
+        <div className='sticky top-0 z-50 flex flex-row justify-between items-center p-4 px-8 border-b border-zinc-800 '>
             <div>
                 Logo
             </div>
             <div className=''>
                 <ul className='flex flex-row gap-10 font-aeonik '>
                     <li>
-                        <Link href="/">Home</Link>
+                        <Link href="/" className={pathName==='/' ? 'font-medium underline underline-offset-8' :''}>Home</Link>
                     </li>
                     <li>
                         <Link href="/projects">Projects</Link>
